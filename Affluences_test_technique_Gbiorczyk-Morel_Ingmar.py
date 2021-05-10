c387b91eb547a7ed5a50a756fa99adccbd2351be
IngmarGbi <ingmar.gbi@gmail.com>
2021-05-10 10:29:37 +0200

    Finalisation du test technique
    1h de travail en plus environ
    2h au total

---
+++ Affluences_test_technique_Gbiorczyk-Morel_Ingmar.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  7 16:19:19 2021
+
+@author: ingma
+"""
+import pandas as pd
+from datetime import datetime, timedelta
+
+#Récupération des données
+data = pd.read_csv("data.csv")
+time = pd.read_csv("timetables.csv")
+
+
+analyse_date = datetime(2021, 5, 6, 16,0,0)
+
+
+#récupération de la dernière donnée par capteur
+#Si un même capteur est placé sur 2 sites qui donnent des informations au même moment, les deux données sont conservées
+dataMaxTime = data.groupby('sensor_identifier')['last_record_datetime'].transform(max) == data['last_record_datetime']
+total = data[dataMaxTime]
+#Jointure avec les horaires d'ouverture
+total = pd.merge(total,time, on="site_id",how="left")
+#Suppression des établissements sans info
+isNa = pd.isna(total['opening_datetime'])
+total= total[~isNa]
+
+#Traitement des données
+for indice,row in total.iterrows():
+    #Transtypage de la date de str en datetime
+    record_datetime = datetime.strptime(row['last_record_datetime'], '%Y-%m-%d %H:%M:%S')
+    delay = analyse_date - record_datetime
+    #Etablissement ouvert
+    if analyse_date > datetime.strptime(row['opening_datetime'], '%Y-%m-%d %H:%M:%S') and analyse_date < datetime.strptime(row['closing_datetime'], '%Y-%m-%d %H:%M:%S') :
+        #Une alerte a lieu 
+        if delay > timedelta(hours = 2) :
+            msg1 = "Sensor "+ row['sensor_name']+" with identifier "+str(row['sensor_identifier'])+ " triggers an alerte at "+str(analyse_date)+" with level"
+            msg2 = "with last data recorded at "+row['last_record_datetime']
+            if delay >  timedelta(days=2) :
+                print(msg1+" 3 "+msg2)
+            elif delay > timedelta(days=1) :
+                print(msg1+" 2 "+msg2)
+            else :
+                print(msg1+" 1 "+msg2)
+            
